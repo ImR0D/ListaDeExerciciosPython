d46c40daf2c9b0264837c06bc79e534fb3c63c8e
ImR0D <gvr.decor@gmail.com>
2022-09-25 00:04:15 -0300

    First version

--- 1. Estrutura Sequencial/ES_07_SquareRoot.py
+++ 1. Estrutura Sequencial/ES_07_SquareRoot.py
@@ -3,27 +3,60 @@
 
 
 # Objetivo do algoritmo é obter a raíz quadrada sem o uso da função sqrt da lib 'math'
-def isSqrt(sqrt, match):
-    m = match ** 2
-    if m <= sqrt:
+
+def isPerfectSquare(_value_):
+    if _value_ is not None and _value_ > 1:
+        if float(_value_) % int(_value_) == 0:
+            return True
+    else:
+        return False
+
+
+def Mult(_value_):
+    return _value_ * _value_
+
+
+def isPair(_value_):
+    if _value_ % 2 == 0:
         return True
     else:
         return False
 
 
-def getSquareRoot(n):
-    x = n/2
-    while (x > 0):
-        x = x / 2.0
-        if not isSqrt(n, x):
-            getSquareRoot(x)
-        else:
-            return x
+def getSquareRoot(_value_):
+    _sqrt_ = float(_value_)
+    if float(_value_) is not None:
+        while Mult(_sqrt_) >= _value_:
+            print(_value_, _sqrt_, Mult(_sqrt_))
+            if isPair(_sqrt_) and Mult(_value_) > _value_:
+                _sqrt_ = _sqrt_ / 2
+            elif not isPair(_sqrt_) and Mult(_value_) > _value_:
+                _sqrt_ = _sqrt_ / 3
+
+            if Mult(_sqrt_) <= _value_:
+                return float(_sqrt_)
+
+    return float(_sqrt_)
+
+
+if __name__ == "__main__":
+    task = float(input("Informe um valor para obter a raíz quadrada: "))
+    r = getSquareRoot(task)
+    if isPerfectSquare(r):
+        print(f"--> Raíz de {task} = {r}. [Quadrado perfeito!]")
+    else:
+        print(f"--> Raíz de {task} = {r}.")
 
 
 
-print("Teste 1 - sqrt -> 8:    ", getSquareRoot(8))
-print("Teste 2 - sqrt -> 28:   ", getSquareRoot(28))
-print("Teste 3 - sqrt -> 1024: ", getSquareRoot(1024))
-print("Teste 4 - sqrt -> 9765: ", getSquareRoot(9765))
-print("Teste 5 - sqrt -> 1000: ", getSquareRoot(1000))
+
+
+
+
+
+
+
+
+
+
+
