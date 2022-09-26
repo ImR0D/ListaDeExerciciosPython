227264585fd05f79f92b39fdeb7aed36c3df8d3b
ImR0D <gvr.decor@gmail.com>
2022-09-26 11:23:08 -0300

    Renamed file and correction exercise

---
+++ 1. Estrutura Sequencial/ES_07_CalcularAreaQuadrado.py
@@ -0,0 +1,24 @@
+# Faça um programa que calcule a área de um quadrado, em seguida mostre
+# o dobro dessa área para o usuário
+
+
+if __name__ == "__main__":
+    size = float(input("Informe o tamanho de um dos lados do quadrado: "))
+
+    area = size ** 2
+
+    print(f"--> Área: {area}")
+    print(f"--> Dobro da Área: {area*2}")
+
+
+
+
+
+
+
+
+
+
+
+
+
